c86c5391ce582e58227b91f25ab41ad74de0f085
chingizagha <cingiz167@gmail.com>
2024-01-08 13:54:45 +0400

    Update

---
+++ I Am Rich/I Am Rich/I_Am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_RichApp.swift
+//  I Am Rich
+//
+//  Created by Chingiz on 08.01.24.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
